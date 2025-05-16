ab1ca5768ed23e54ee0e748e5294822aba537674
Priyanka <priyanka.rel6@gmail.com>
2025-05-16 12:05:21 +0530

    created a file hello world

--- 02-Addition.sh
+++ 02-Addition.sh
@@ -3,5 +3,7 @@
 NUMBER1=$1
 NUMBER2=$2
 
+TIMESTAMP=$(date)
+    echo "(Script executed at : $TIMESTAMP)"
 SUM=$(($NUMBER1+$NUMBER2))
-echo "(Sum of Number1 and Number2 : $SUM)"
+    echo "(Sum of Number1 and Number2 : $SUM)"
