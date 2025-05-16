9ca775a77fb554ded14965eed0886b68d41db2de
Priyanka <priyanka.rel6@gmail.com>
2025-05-16 12:15:52 +0530

    created a file hello world

--- 02-Array.sh
+++ 02-Array.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 MOVIES"(POKIRI, OKKADU, ATHADU, DOOKUDU)"
-    echo"(First movie : $MOVIES[0])"
-    echo"(Second movie : $MOVIES[1])"
-    echo"(Third movie : $MOVIES[2])"
-    echo"(Fourth movie : $MOVIES[3])"
-    
+    echo "(First movie : $(MOVIES[0]))"
+    echo "(Second movie : $(MOVIES[1]))"
+    echo "(Third movie : $(MOVIES[2]))"
+    echo "(Fourth movie : $(MOVIES[3]))"
