b16832f5b617436bca21cf376c5d7626cfd0a36c
Priyanka <priyanka.rel6@gmail.com>
2025-05-26 19:28:24 +0530

    created script file for user cart and redis

---
+++ disk-usage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # in project it will be 75
+MSG=""
+IP=$(http://169.254.169.254/latest/meta-data/local-ipv4)
+
+while IFS= read line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
+    fi
+done <<< $DISK_USAGE
+
+#echo -e $MSG
+
+sh mail.sh "Mohan Vamsi" "High Disk Usage" "$IP" "$MSG" "mohanvamsi011@gmail.com" "ALERT-High Disk Usage"
